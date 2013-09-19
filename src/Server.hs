4b00e0b57ac5a1427fd8eacf1e8610a716fc1c95
Maxim Dikun <dikmax@gmail.com>
2013-09-19 22:43:41 +0300

    GitHub webhook
    Former-commit-id: 0d2e579c0a93167688dee1b175adae16b4689459

--- src/Server.hs
+++ src/Server.hs
@@ -1,13 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import           Snap.Core
+import           Control.Applicative
+import           Control.Monad.IO.Class
+import           Data.ByteString
+import qualified Data.ByteString.Char8 as BS
 import           FileServe
+import           Snap.Core
 import           Snap.Http.Server
+import           System.Process
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site = serveDirectory "_site"
+site = path "github-webhook" githubWebhook <|>
+    serveDirectory "_site"
 
+githubWebhook :: Snap ()
+githubWebhook = do
+  request <- getRequest
+  let ip = rqRemoteAddr request
+  if ip == "127.0.0.1" || "204.232.175." `isPrefixOf` ip || "192.30.252." `isPrefixOf` ip
+    then do
+      output <- liftIO $ readProcess "./update.sh" [] ""
+      writeBS $ BS.pack output
+    else writeBS "Fail"
