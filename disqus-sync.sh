bb90c8b1684b233dafc2419e44869778105f856b
Maxim Dikun <dikmax@gmail.com>
2013-10-11 09:29:41 +0300

    Sync script
    Former-commit-id: ad17cde9367eb2aa19f8097e375515c9687da2b7

--- disqus-sync.sh
+++ disqus-sync.sh
@@ -1,3 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-git pu
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd ${DIR}
+git pull
+cd disqus-sync
+node sync.js
+cd ..
+git add comments/*.html
+git commit -m "Comments sync"
+git push
