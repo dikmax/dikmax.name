1492e3983c55b96e2847ab217cfb14ab1a5f1e89
Maxim Dikun <dikmax@gmail.com>
2013-10-13 12:28:19 +0300

    New JS
    Former-commit-id: 2350e19c4a4c8af3bd4f2b25b13f3ea182a58143

---
+++ build-scripts.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+echo "Building highlight.js..."
+python js/highlight.js/tools/build.py -n bash css haskell javascript php sql xml
+echo 'goog.provide("hljs");' > js/highlight.pack.js
+cat js/highlight.js/build/highlight.pack.js >> js/highlight.pack.js
+
+echo "Building templates..."
+for file in $(find js/dikmax -iname "*.soy")
+do
+  echo $file
+  java -jar js/closure-templates/SoyToJsSrcCompiler.jar --shouldGenerateJsdoc --shouldProvideRequireSoyNamespaces --outputPathFormat {INPUT_DIRECTORY}/{INPUT_FILE_NAME_NO_EXT}.js $file
+done
+
+echo "Updating dependencies..."
+js/closure-library/closure/bin/build/depswriter.py \
+  --root_with_prefix="js/closure-library/third_party/closure/goog ../../third_party/closure/goog" \
+  --root_with_prefix="js/dikmax ../../../dikmax" \
+  --root=js/closure-library/closure/goog \
+  --path_with_depspath="js/closure-templates/soyutils_usegoog.js ../../../closure-templates/soyutils_usegoog.js" \
+  --path_with_depspath="js/highlight.pack.js ../../../highlight.pack.js" \
+  --output_file=js/deps.js
+
+echo "Compiling script..."
+js/closure-library/closure/bin/build/closurebuilder.py \
+  --root=js/dikmax/ \
+  --root=js/closure-library/closure/goog/ \
+  --root=js/closure-library/third_party/closure/goog/ \
+  --namespace="dikmax.main" \
+  --output_mode=compiled \
+  --compiler_jar=js/closure-compiler/compiler.jar \
+  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
+  --compiler_flags="--use_types_for_optimization" \
+  --compiler_flags="--warning_level=VERBOSE" \
+  --compiler_flags="--charset=UTF-8" \
+  --compiler_flags="--create_source_map=js/script.js.map" \
+  js/closure-templates/soyutils_usegoog.js \
+  js/highlight.pack.js \
+  > js/script.js
+
+# echo "Compiling testing version..."
+# js/closure-library/bin/build/closurebuilder.py \
+#  --root=static/js/dikmax/ \
+#  --root=static/js/goog/ \
+#  --root=static/js/soy/ \
+#  --root=static/js/third_party/ \
+#  --namespace="dikmax.main" \
+#  --output_mode=compiled \
+#  --compiler_jar=js/closure-compiler/compiler.jar \
+#  --compiler_flags="--externs=closure/compiler/externs/webkit_console.js" \
+#  --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
+#  --compiler_flags="--use_types_for_optimization" \
+#  --compiler_flags="--warning_level=VERBOSE" \
+#  --compiler_flags="--debug" \
+#  --compiler_flags="--formatting=PRETTY_PRINT" \
+#  --compiler_flags="--formatting=PRINT_INPUT_DELIMITER" \
+#  --compiler_flags="--charset=UTF-8" \
+#  --compiler_flags="--create_source_map=static/js/script.testing.js.map" \
+#  --compiler_flags="--define='EXCLUDE_VAULT=true'" \
+#  static/js/highlight.pack.js \
+#  > static/js/script.testing.js
