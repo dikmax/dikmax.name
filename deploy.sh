1adc7da3d9dce1c20994d766467f870bc16606db
Maxim Dikun <dikmax@gmail.com>
2014-08-06 10:17:46 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-rsync --recursive --verbose --delete --force --compress _site 188.226.255.34:/home/dikmax/dikmax.name
+git pull
+dist/build/dikmax-name/dikmax-name build
+rsync --recursive --delete --force --compress --progress --iconv=UTF8-MAC,UTF-8 _site 188.226.255.34:/home/dikmax/dikmax.name
