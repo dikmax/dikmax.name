095da7ad4e091f87f5b293cf40ebfb537d8d5ec1
Maxim Dikun <dikmax@gmail.com>
2014-08-16 23:42:59 +0300

    Less compiler

--- src/Site.hs
+++ src/Site.hs
@@ -31,6 +31,7 @@ import           XmlHtmlWriter
 main :: IO ()
 main = hakyll $ do
     staticFilesRules
+    lessCompilerRules
     commentsRules
     postsRules
     tagsPagesRules
@@ -266,6 +267,24 @@ staticFilesRules = do
         ]) $ do
         route   idRoute
         compile copyFileCompiler
+
+
+--------------------------------------------------------------------------------
+-- LESS files
+--------------------------------------------------------------------------------
+
+lessCompilerRules :: Rules ()
+lessCompilerRules = do
+    match "less/*.less" $
+        compile getResourceBody
+
+    d <- makePatternDependency "less/**"
+    rulesExtraDependencies [d] $ create ["css/style.css"] $ do
+        route idRoute
+        compile $ loadBody "less/style.less"
+            >>= makeItem
+            >>= withItemBody
+              (unixFilter "lessc" ["--clean-css","-O2", "--include-path=less","-"]) -- TODO replace with --compress or --clean-css
 
 --------------------------------------------------------------------------------
 -- Posts
