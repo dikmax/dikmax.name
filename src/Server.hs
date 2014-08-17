74ae85618856689063c35a1f8b5b66ff6336f8d0
Maxim Dikun <dikmax@gmail.com>
2014-08-17 19:55:58 +0300

    Remove GitHub webhook

--- src/Server.hs
+++ src/Server.hs
@@ -2,35 +2,20 @@
 module Main where
 
 import           Control.Applicative
-import           Control.Monad.IO.Class
-import           Data.ByteString
-import qualified Data.ByteString.Char8 as BS
 import           FileServe
 import           Snap.Core
 import           Snap.Http.Server
-import           System.Process
 
 main :: IO ()
 main = quickHttpServe site
 
 site :: Snap ()
-site = path "github-webhook" githubWebhook <|>
-    path "rss" (redirect "feed.rss") <|>
+site = path "rss" (redirect "feed.rss") <|>
     path "rss/" (redirect "feed.rss") <|>
     dir "media" (serveDirectory "/home/dikmax/Dropbox/dikmax.name") <|>
     serveDirectory "_site" <|>
     notFoundHandler
 
-
-githubWebhook :: Snap ()
-githubWebhook = do
-  request <- getRequest
-  let ip = rqRemoteAddr request
-  if ip == "127.0.0.1" || "204.232.175." `isPrefixOf` ip || "192.30.252." `isPrefixOf` ip
-    then do
-      output <- liftIO $ readProcess "./update.sh" [] ""
-      writeBS $ BS.pack output
-    else writeBS "Fail"
 
 notFoundHandler :: Snap ()
 notFoundHandler = do
