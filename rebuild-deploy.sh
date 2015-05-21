043805eb97b2f95778c5a8a08c65c589692555d0
Maxim Dikun <dikmax@gmail.com>
2015-05-21 12:59:38 +0300

    Add rebuild-deploy script

---
+++ rebuild-deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git pull
+git submodule update
+dist/build/dikmax-name/dikmax-name rebuild
+pub global run static_compress --dir _site
+rsync --recursive --delete --force --compress --progress --iconv=UTF8-MAC,UTF-8 _site dikmax.name:/home/dikmax/dikmax.name
